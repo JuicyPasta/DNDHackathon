0135adab9d83555275012bcac1df65e3940e81a5
Jackson Darrow <jackson.darrow@gmail.com>
2016-04-09 12:46:39 -0700

    saving

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require("fs");
-var urls = {}
+var urls = {};
 
 var polyform = "http://polyratings.com/search.phtml?";
 
