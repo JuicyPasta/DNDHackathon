b528555b284886f9ddfab00bda3831f3bd86e420
Jackson Darrow <jackson.darrow@gmail.com>
2016-04-09 12:38:52 -0700

    added graph

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     res.render('home');
 });
 
-router.get('/', function(req, res, next) {
+router.get('/graph', function(req, res, next) {
     res.render('graph');
 });
 
