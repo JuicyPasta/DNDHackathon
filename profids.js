97afcac5b8f69512986b797fc53b49636b9c1c81
Jackson Darrow <jackson.darrow@gmail.com>
2016-04-09 15:53:57 -0700

    profsid

---
+++ profids.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require("fs");
+var urls = {};
+
+var formurl = "http://polyratings.com/list.phtml";
+var profs = {};
+
+request(formurl, function (error, response, body) {
+   if(!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var hrefs = $("a");
+      var profIds = {};
+      console.log(hrefs[5].attribs.href);
+      for(var i = 5; i < hrefs.length - 1; i++) {
+        var name = hrefs[i].children[0].data; 
+        name = name.split(', ');
+        name = name.join('');
+
+         profs[name] = hrefs[i].attribs.href.split("=")[1];
+      }
+   }
+});
+
+module.exports = function getId(name) {
+    return profs[name];
+};
