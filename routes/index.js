1368e63aafe7a5f449961b2508add48bda01405e
mrgoodrich <mryangoodrich@icloud.com>
2016-04-09 01:42:15 -0700

    stuff

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index');
+    res.render('home');
 });
 
-router.get('/teacher/.*fly$/', function(req, res, next) {
-    console.log('/.*fly$/');
 
-    res.render('index', { title: 'Express' });
-});
+
 
 module.exports = router;
