edbb327a29ebd138dde20fb9fdca68f9e75f9bdf
Jackson Darrow <jackson.darrow@gmail.com>
2016-04-08 22:23:18 -0700

    added error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-      res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+router.get('/teacher/.*fly$/', function(req, res, next) {
+    console.log('/.*fly$/');
+
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
