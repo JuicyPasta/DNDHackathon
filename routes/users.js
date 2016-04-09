fb4a157e8c9dafac45b55695fbf33db6a296d2e2
Jackson Darrow <jackson.darrow@gmail.com>
2016-04-08 22:33:22 -0700

    added error

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-      res.send('respond with a resource');
+    var url = "http://polyratings.com/stats.phtml?profid=778";
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    });
+
+    res.render('index');
 });
 
 module.exports = router;
