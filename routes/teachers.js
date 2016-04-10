eb39e1275370c362e8799fcd4118342726655b1a
mrgoodrich <mryangoodrich@icloud.com>
2016-04-09 18:58:56 -0700

    showing teacher in search

---
+++ routes/teachers.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var teachersearch = require('../routes/teachersearch.js');
+
+var fs = require('fs');
+
+router.post('/teacherQuery/:search', function(req, res, next) {
+   console.log("received teacher query in teachers.js");
+   var teacherMap = teachersearch(req.params.searchString);
+    // console.log(teacherMap);
+   var matchTeachers = { };
+   var teachers = Object.keys(teacherMap);
+   var searchString = req.params.search;
+   // console.log("lenght of teachgers is " + teachers.length);
+   for (teacherNdx = 0; teacherNdx < teachers.length; teacherNdx++) {
+      // console.log("teacherNdx is " + teacherNdx);
+      var teacherName = teachers[teacherNdx];
+      // console.log("teacher name is " + teacherName);
+      // console.log("lower case name is " + teacherName.toLowerCase());
+      // console.log("lower case search string is " + searchString.toLowerCase());
+      // console.log("checking if teacher |" + teacherName.toLowerCase() + "| contains " + searchString.toLowerCase());
+      if (teacherName.toLowerCase().indexOf(searchString.toLowerCase()) > -1) {
+         // console.log(teacherName);
+         matchTeachers[teacherName] = teacherMap[teacherName];
+         
+      }
+   }
+   // console.log('in teahcerQuery map is ' + teacherMap);
+   console.log("FINAL MATTCH TEACHERS IS " + matchTeachers);
+   res.send(matchTeachers);
+});
+
+
+module.exports = router;
--- routes/teachersearch.js
+++ routes/teachersearch.js
@@ -1,26 +1,32 @@
-var express = require("express");
-var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require("fs");
-var teachers = require("../profids.js");
+var urls = {};
 
-router.get(/\/.*/, function (searchStr) {
-      var matchedTeachers = {};
-      var index = 0;
-      
-      searchStr = "Johnson";
+var formurl = "http://polyratings.com/list.phtml";
+var profs = {};
 
-      for(var i = 0; i < teachers.length; i++) {
-       
-         if(teachers[i].indexOf(searchStr) > -1) {
-            matchedTeachers[index] = teachers[i];
-            console.log(matchedTeachers[index]);
-            index++;
-         }
+request(formurl, function (error, response, body) {
+   if(!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var hrefs = $("a");
+      var profIds = {};
+      console.log(hrefs[5].attribs.href);
+      for(var i = 5; i < hrefs.length - 1; i++) {
+        var name = hrefs[i].children[0].data; 
+        name = name.split(', ');
+        name = name.join('');
+
+         profs[name] = hrefs[i].attribs.href.split("=")[1];
       }
-
-      return matchedTeachers;
+   }
 });
 
-module.exports = router;
+profs.GerfenJeffrey = 946;
+profs.MammenKurt = 970;
+profs.KearnsTimothy = 334;
+profs.JaniMeera = 4056;
+
+module.exports = function getId(name) {
+    return profs;
+};
