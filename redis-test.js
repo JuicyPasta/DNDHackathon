8838cad9a137f18c2985fe35ec8e6c8ecafa5927
juicypasta <jackson.darrow@gmail.com>
2016-05-15 10:00:12 +0000

    redis almost done with the chrome extension

--- redis-test.js
+++ redis-test.js
@@ -1,4 +1,4 @@
-var endpoint = "extensiondata-001.p4b2ml.0001.usw1.cache.amazonaws.com";
+var endpoint = "127.0.0.1";
 
 var redis = require("redis"),
     client = redis.createClient(6379, endpoint, {no_ready_check: true});
@@ -9,30 +9,21 @@ client.on('connect', function () {
 });
 
 client.on('error', function (err) {
+    console.log("ERR:");
     console.log(err);
 });
 
-client.on('ready', function () {
-    console.log("ready");
-});
+client.set("test", "OK");
 
-client.on('end', function () {
-    console.log("end");
-});
-
-client.set("foo_rand000000000000", "OK");
- 
 // This will return a JavaScript String 
-client.get("foo_rand000000000000", function (err, reply) {
-    console.log(err);
-        console.log(reply); // Will print `OK` 
+client.get("test", function (err, reply) {
+    console.log(reply); // Will print `OK` 
 });
- 
-// This will return a Buffer since original key is specified as a Buffer 
-client.get(new Buffer("foo_rand000000000000"), function (err, reply) {
+
+client.get("not there", function (err, reply) {
     console.log(err);
-        console.log(reply); // Will print `<Buffer 4f 4b>` 
+    console.log(reply);
 });
+
 
 client.quit();
-
--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,44 @@ var request = require('request');
 var cheerio = require('cheerio');
 var profsids = require('../modules/index-teachers.js');
 var scraper = require('../modules/comment-scrape.js');
-
+var redis = require('redis');
 var fs = require('fs');
+
+var redisEndpoint = '127.0.0.1';
+var client = redis.createClient(6379, redisEndpoint, {no_ready_check: true});
+
+var baseStatUrl = "http://polyratings.com/stats.phtml?profid=";
 
 /* GET users listing. */
 router.get(/\/.*/, function(req, res, next) {
+    console.log("HI");
     var originalUrl = req.originalUrl;
     var teacher = originalUrl.split('/')[2];
     var profId = profsids.getId(teacher);
 
-    console.log(profId);
-    var url = "http://polyratings.com/stats.phtml?profid=" + profId;
 
+    if (!profId) {
+        return res.send(500);
+    }
+
+    client.get(profId, (err, data) => {
+        if (err) {
+            res.send(500);
+        } else if (data) {
+            console.log("found old data!");
+            res.send(data);
+        } else {
+            console.log("making new data");
+
+            scrapeData(profId, (err, data) => {
+                client.set(profId, data);
+            });
+        }
+    });
+});
+
+function scrapeData(profId, cb) {
+    var url = baseStatUrl + profId;
     var data = {
         found:false,
         id:profId,
@@ -50,61 +76,56 @@ router.get(/\/.*/, function(req, res, next) {
         },
     };
 
-    if (profId) {
-        if (fs.existsSync('./photos/' + data.teacher.split(' ').join('') + '.jpg')) {
-            data.photo = data.teacher.split(' ').join('') + ".jpg";
-            console.log("photo " + data.photo);
-        }
-        if (fs.existsSync('./clouds/' + data.teacher.split(' ').join('') + 'Cloud.svg')) {
-            data.cloud = data.teacher.split(' ').join('') + "Cloud.svg";
-            console.log("cloud " +data.cloud);
-        }
+    if (fs.existsSync('./photos/' + data.teacher.split(' ').join('') + '.jpg')) {
+        data.photo = data.teacher.split(' ').join('') + ".jpg";
+        console.log("photo " + data.photo);
+    }
+    if (fs.existsSync('./clouds/' + data.teacher.split(' ').join('') + 'Cloud.svg')) {
+        data.cloud = data.teacher.split(' ').join('') + "Cloud.svg";
+        console.log("cloud " +data.cloud);
+    }
 
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
-                $ = cheerio.load(body);
-                var tds = $('td');
-                if (tds[28] && tds[28].children[0].data.trim() !== undefined) {
-                    data.found = true;
-                    data.totalEvals = tds[28].children[0].data.trim();
-                    data.overall.mean = tds[30].children[0].data;
-                    data.overall.mode = tds[34].children[0].data;
-                    data.overall.stdDev = tds[36].children[0].data;
+            $ = cheerio.load(body);
+            var tds = $('td');
+            if (tds[28] && tds[28].children[0].data.trim() !== undefined) {
+                data.found = true;
+                data.totalEvals = tds[28].children[0].data.trim();
+                data.overall.mean = tds[30].children[0].data;
+                data.overall.mode = tds[34].children[0].data;
+                data.overall.stdDev = tds[36].children[0].data;
 
-                    data.presentation.mean = tds[40].children[0].data;
-                    data.presentation.mode = tds[44].children[0].data;
-                    data.presentation.stdDev = tds[46].children[0].data;
+                data.presentation.mean = tds[40].children[0].data;
+                data.presentation.mode = tds[44].children[0].data;
+                data.presentation.stdDev = tds[46].children[0].data;
 
-                    data.understandingness.mean = tds[50].children[0].data;
-                    data.understandingness.mode = tds[54].children[0].data;
-                    data.understandingness.stdDev = tds[56].children[0].data;
+                data.understandingness.mean = tds[50].children[0].data;
+                data.understandingness.mode = tds[54].children[0].data;
+                data.understandingness.stdDev = tds[56].children[0].data;
 
-                    var deptSelect = $('font');
-                    data.department = deptSelect[3].children[0].data;
-                    data.teacher = deptSelect[2].children[0].data;
-                    console.log(data.teacher);
+                var deptSelect = $('font');
+                data.department = deptSelect[3].children[0].data;
+                data.teacher = deptSelect[2].children[0].data;
+                console.log(data.teacher);
 
-                    var thing = $('u');
+                var thing = $('u');
 
-                    var j = 3;
-                    while ((thing[j].children[0].data).indexOf("Receiving") > -1 && data.grades.hasOwnProperty(thing[j].children[0].data.split("'")[1])) {
-                        data.grades[thing[j].children[0].data.split("'")[1]] = tds[28 + 11 * j].children[0].data;
-                        j++;
-                    }
+                var j = 3;
+                while ((thing[j].children[0].data).indexOf("Receiving") > -1 && data.grades.hasOwnProperty(thing[j].children[0].data.split("'")[1])) {
+                    data.grades[thing[j].children[0].data.split("'")[1]] = tds[28 + 11 * j].children[0].data;
+                    j++;
                 }
-
-
-                res.send(data);
-
             }
-        });
-    }
-    else {
-        res.send(data);
-    }
 
-});
+            cb(null, data);
 
+        } else {
+            cb(error);
+        }
+    });
+
+}
 
 module.exports = router;
